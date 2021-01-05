3f93d50d92950c53c9bae0c7cdf2a5b43a8f34a1
Kim Yin <kyiimn@gmail.com>
2021-01-05 16:21:03 +0900

    DB스키마 작업중

---
+++ schema/tbl_account.sql
@@ -0,0 +1,8 @@
+-- DROP TABLE t_account_dept;
+CREATE TABLE t_account_dept (
+) WITH (OIDS = FALSE);
+
+-- DROP TABLE t_account_user;
+CREATE TABLE t_account_user (
+) WITH (OIDS = FALSE);
+
---
+++ schema/tbl_publish.sql
@@ -0,0 +1,36 @@
+-- DROP TABLE t_page_publish_info;
+CREATE TABLE t_page_publish_info (
+) WITH (OIDS = FALSE);
+
+-- DROP TABLE t_page_object;
+CREATE TABLE t_page_object (
+) WITH (OIDS = FALSE);
+
+-- DROP TABLE t_page_adver;
+CREATE TABLE t_page_adver (
+) WITH (OIDS = FALSE);
+
+-- DROP TABLE t_page_title;
+CREATE TABLE t_page_title (
+) WITH (OIDS = FALSE);
+
+-- DROP TABLE t_page;
+CREATE TABLE t_page (
+) WITH (OIDS = FALSE);
+
+-- DROP TABLE t_page_pattern;
+CREATE TABLE t_page_pattern (
+) WITH (OIDS = FALSE);
+
+-- DROP TABLE t_page_template;
+CREATE TABLE t_page_template (
+) WITH (OIDS = FALSE);
+
+-- DROP TABLE t_page_print;
+CREATE TABLE t_page_print (
+) WITH (OIDS = FALSE);
+
+-- DROP TABLE t_page_week_plan;
+CREATE TABLE t_page_week_plan (
+) WITH (OIDS = FALSE);
+
